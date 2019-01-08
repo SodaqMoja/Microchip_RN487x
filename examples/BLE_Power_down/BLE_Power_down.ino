4f94c0fb59b977f15a0cb82e8d6e366f256e84c6
Gabriel Notman <g.notman@gmail.com>
2019-01-08 17:27:29 +0100

    Added Power down example (dormant mode)

---
+++ examples/BLE_Power_down/BLE_Power_down.ino
@@ -0,0 +1,54 @@
+// BLE_Power_down.ino
+// Author: Gabriel Notman
+// Date: 2019/01/8
+
+/*
+    (c) 2016 Microchip Technology Inc. and its subsidiaries. You may use this
+    software and any derivatives exclusively with Microchip products.
+
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+    WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+    PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION
+    WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION.
+
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+    BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+    FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+    ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+    THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+
+    MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE
+    TERMS.
+*/
+
+#include <Arduino.h>
+#include <RN487x_BLE.h>
+
+#define STARTUP_DELAY   5000
+#define bleSerial       Serial1
+
+void setup() 
+{
+  delay(STARTUP_DELAY);
+
+  // Start the BT Serial to send the commands
+  rn487xBle.hwInit();
+  bleSerial.begin(rn487xBle.getDefaultBaudRate());
+  rn487xBle.initBleStream(&bleSerial);
+
+  // Enter dormant mode
+  rn487xBle.enterCommandMode();
+  rn487xBle.dormantMode();
+
+  // It is safe to turn off the UART
+  bleSerial.end();
+}
+
+void loop() 
+{
+}
+
+
